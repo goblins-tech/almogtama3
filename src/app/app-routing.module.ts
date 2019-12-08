96a273e50b94ee4c1ac13d6cb74f32331185f1fa
admin <admin@almogtama3.com>
2019-12-08 17:33:09 +0200

    ..

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ContentComponent } from "./content/content.component";
+import { ErrorComponent } from "./error/error.component";
+
+const routes: Routes = [
+  {
+    path: ":type/:id",
+    component: ContentComponent
+  },
+  {
+    path: "**",
+    component: ErrorComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
