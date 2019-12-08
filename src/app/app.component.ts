6e8dd7f90630ffaffe9077a1ba537ddd149486e1
admin <admin@almogtama3.com>
2019-12-08 19:30:51 +0200

    content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'almogtama3';
+export class AppComponent implements OnInit {
+  title = "almogtama3";
+  constructor(private router: Router) {}
+  ngOnInit() {}
+  go(path: Array | string) {
+    if (typeof path == "string") path = [path];
+    this.router.navigate(path);
+  }
 }
