85188da7a976b71cc28b8d9726a1e531d94a09f4
admin <admin@almogtama3.com>
2020-01-12 18:49:07 +0200

    ..

--- start-dev.bat
+++ start-dev.bat
@@ -1,4 +1,4 @@
-start "build" npm run start:dev
+start /w /b "build" npm run start:dev
 
 ::rebuild browser: npm run build:browser:dev && npm run serve
 ::rebuild server: npm run build:server:dev && npm run webpack:server && npm run serve
