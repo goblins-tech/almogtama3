4af76663a35f1b23654e31c7c2625fd283cdbd1d
admin <admin@almogtama3.com>
2020-05-13 13:44:55 +0200

    adsense
    - load adsense dynamically, because Angular dosen't allow to insert 
    <script> tag inside the template.

--- packages/ngx-adsense/component.ts
+++ packages/ngx-adsense/component.ts
@@ -6,20 +6,38 @@ import {
   QueryList,
   Input,
   Output,
-  EventEmitter
+  EventEmitter,
+  ElementRef,
+  AfterViewInit,
+  Inject
 } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
 
+/*
+angular dosen't allow to use <script> inside the template, so we dynamically load it.
+ */
 @Component({
   selector: "ngx-adsense",
-  template: `
-    <script
-      id="adsense"
-      data-ad-client="{{ id }}"
-      async
-      src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-    ></script>
-  `
+  template: ``
 })
-export class NgxAdsenseComponent {
+export class NgxAdsenseComponent implements AfterViewInit {
   @Input() id: string; //ca-pub-**
+  @Input() src: string;
+  constructor(
+    @Inject(DOCUMENT) private document,
+    private elementRef: ElementRef
+  ) {}
+
+  ngAfterViewInit() {
+    var s = this.document.createElement("script");
+    s.type = "text/javascript";
+    s["data-ad-client"] = this.id;
+    s.src =
+      this.src || "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+    //no need to use s.async=true; because dynamically loaded scripts are async by default
+    // s.onload = function () { __this.afterScriptAdded(); }; //__this=this
+
+    //todo: or add to document.head
+    this.elementRef.nativeElement.appendChild(s);
+  }
 }
