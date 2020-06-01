d19ee528866ad54651abea70ac0716b245c35202
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-06-01 18:56:49 +0200

    [nodejs-tools] support type=module

--- packages/ngx-tools/load-scripts.service.ts
+++ packages/ngx-tools/load-scripts.service.ts
@@ -14,7 +14,10 @@ export class NgxToolsLoadService {
     cb?: (type: string) => void,
     parent? //todo: HtmlElement
   ) {
-    if (type == "css") {
+    //tmp: for index.html
+    //if (!cb) cb = ev => console.log("[load]", ev, src);
+
+    if (type === "css") {
       type = "link";
       attributes.rel = "stylesheet";
       attributes.type = "text/css";
@@ -23,21 +26,23 @@ export class NgxToolsLoadService {
     if (type === "link") {
       attributes.href = src;
       attributes.crossorigin = true; //https://developer.mozilla.org/en-US/docs/Web/HTML/Preloading_content#Cross-origin_fetches
-    } else if (type == "script") {
+    } else if (type === "script" || type === "module") {
       attributes.src = src;
-      attributes.type = "text/javascript";
+      attributes.type = type === "script" ? "text/javascript" : type;
     }
 
     if (!("async" in attributes)) attributes.async = true;
 
-    let el = this.document.createElement(type);
+    let el = this.document.createElement(type === "link" ? "link" : "script");
     for (let key in attributes) {
       el.setAttribute(key, attributes[key]);
     }
 
     if (cb) {
-      el.onload = () => cb("loaded");
-      el.onreadystatechange = () => cb("ready");
+      //or return a promise -> el.onLoad=resolve
+      el.addEventListener("load", () => cb("loaded"));
+      el.addEventListener("readystatechange", () => cb("ready"));
+      el.addEventListener("error", () => cb("error"));
     }
 
     (parent || this.document.getElementsByTagName("head")[0]).appendChild(el);
