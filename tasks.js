b6aa952e022a3b9f44a692889d6de6cb41ad14a2
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-06-02 21:54:03 +0200

    [tasks] optimize

--- tasks.js
+++ tasks.js
@@ -3,82 +3,6 @@ const { execSync } = require("child_process");
 const { argv } = require("process");
 const jsDom = require("jsdom").JSDOM;
 
-var tasks = {
-  terser: function(dir = "./dist") {
-    fs.readdirSync(dir).forEach(el => {
-      let newDir = `${dir}/${el}`;
-      if (fs.statSync(newDir).isDirectory()) this.terser(newDir);
-      else {
-        //todo: terser runtime-es2015 -> error: ngDevMode is not defined
-        if (newDir.endsWith(".js") && !newDir.includes("main-es2015.")) {
-          console.log(`>> terser ${newDir}`);
-          execSync(
-            `npx terser ${newDir} --output ${newDir} --compress --mangle --keep-fnames --ie8 --safari10`
-          );
-        }
-      }
-    });
-  },
-  /*
-   from cmd:
-    - linux cp firebase/package.json dist/package.json
-    - windows copy firebase\package.json dist\package.json
-   */
-  "firebase:copy": function(src = ".", dist = "./dist") {
-    //fs.copyFileSync(`${src}/package.json`, `${dist}/package.json`);
-    let package = JSON.parse(
-      fs.readFileSync(`${src}/package.json`, "utf8").toString()
-    );
-    package.main = package.main.replace("dist/", "");
-    fs.writeFileSync(`${dist}/package.json`, JSON.stringify(package));
-  },
-  transformIndex(path = "./dist/browser/index.html") {
-    //DOMParser() is not available in nodejs, so we use `jsdom`
-    let content = fs.readFileSync(path, "utf8").toString();
-    fs.writeFileSync(path + ".bkp", content);
-
-    let dom = new jsDom(content).window.document;
-    let mainScript = dom.getElementById("main-script");
-    let txt = "";
-
-    dom.querySelectorAll("script").forEach(script => {
-      //todo: or class: keep
-      if (!script.src) return;
-
-      let type = script.getAttribute("type");
-
-      //todo: converting <script type="module"> to load() causes a blank page displayed.
-      //even if they loaded.
-      if (type == "module") return;
-
-      /*
-        - nomodule prevents the modern browsers to load the script,
-          it instead, will load the "module" version
-          https://stackoverflow.com/a/45947601/12577650
-       */
-
-      txt += `load("${script.src}","${type || "script"}",{${
-        type === "module" ? "" : "nomodule:true,defer:true"
-      }});\n`;
-
-      script.remove();
-      //
-    });
-
-    dom.querySelectorAll("link").forEach(el => {
-      if (el.rel !== "stylesheet") return;
-      txt += `load("${el.href}","css");`;
-      el.remove();
-    });
-
-    txt = `document.addEventListener("load", () => {${txt}});`;
-    dom.createTextNode(txt);
-    mainScript.append(txt);
-
-    fs.writeFileSync(path, dom.documentElement.outerHTML);
-  }
-};
-
 //todo: use async : if(isPromise(result))result.then->log(done)
 const task = argv.slice(2)[0];
 if (!task) console.error("enter a task");
@@ -92,3 +16,85 @@ else
   } catch (err) {
     console.error(`>> error in task ${task}\n`, err);
   }
+
+let tasks = {
+  optimize: function(options = {}) {
+    //optimize the bundle for production mode
+
+    /*
+     copy package.json to ./dist for firebase
+     from cmd:
+      - linux cp firebase/package.json dist/package.json
+      - windows copy firebase\package.json dist\package.json
+      */
+    let package = JSON.parse(
+      fs.readFileSync(`./package.json`, "utf8").toString()
+    );
+    package.main = package.main.replace("dist/", "");
+    fs.writeFileSync(`${dist}/package.json`, JSON.stringify(package));
+
+    //minify js files using terser
+    //todo: minifying main-es2015.*.js causes 'null injector error' in /$type/editor
+    let terser = function(dir) {
+      fs.readdirSync(dir)
+        .filter(el => el.endsWith(".js") && !el.includes("main-es2015."))
+        .forEach(el => {
+          el = `${dir}/${el}`;
+          if (fs.statSync(el).isDirectory()) return this.terser(el);
+
+          //todo: terser runtime-es2015 -> error: ngDevMode is not defined
+
+          console.log(`>> terser ${el}`);
+          execSync(
+            `npx terser ${el} --output ${el} --compress --mangle --keep-fnames --ie8 --safari10`
+          );
+        });
+    };
+    terser("./dist");
+
+    //transform index.html (lazy-load resources, and move them after 'load' event)
+    //DOMParser() is not available in nodejs, so we use `jsdom`
+    let content = fs.readFileSync(path, "utf8").toString();
+    fs.writeFileSync(path + ".bkp", content);
+
+    let dom = new jsDom(content).window.document,
+      mainScript = dom.getElementById("main-script"),
+      txt = "";
+
+    dom.querySelectorAll("script").forEach(script => {
+      if (!script.src) return; //todo: ||classList.includes["keep"]
+
+      //todo: converting <script type="module"> to load() causes a blank page displayed.
+      //even if they loaded.
+      //let type = script.getAttribute("type");
+      if (type == "module") return;
+
+      /*
+           - nomodule prevents the modern browsers to load the script,
+             it instead, will load the "module" version
+             https://stackoverflow.com/a/45947601/12577650
+          */
+
+      txt += `load("${script.src}","${type || "script"}",{${
+        type === "module" ? "" : "nomodule:true,defer:true"
+      }});\n`;
+
+      script.remove();
+    });
+
+    dom.querySelectorAll("link").forEach(el => {
+      if (el.rel !== "stylesheet") return;
+      txt += `load("${el.href}","css");`;
+      el.remove();
+    });
+
+    txt = `document.addEventListener("load", () => {${txt}});`;
+    dom.createTextNode(txt);
+    mainScript.append(txt);
+
+    fs.writeFileSync(path, dom.documentElement.outerHTML);
+
+    //the hashes for modified files is changed, so we need to rebuild ngsw-config with the new hashes
+    execSync(`npx ngsw-config dist/browser ngsw-config.json`);
+  }
+};
